ff607bc2710a2b55a17f21ea8d0073234e824a44
kevml <65592475+kevml@users.noreply.github.com>
2020-11-10 14:43:46 +0100

    Video Nr. 8 changes

--- django_project/urls.py
+++ django_project/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from users import views as user_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls), #chops of "admin/"-String from the URL rest goes to admin.site.urls
@@ -27,6 +29,8 @@ urlpatterns = [
     path('', include('blog.urls')),
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # Damit Bilder im Browser angezeigt werden können
 
 
 
--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User #User-Modell wird von Django zur Verfügung gestellt
 
 # Create your models here.
+
+
+class Profile(models.Model): # erbt von import. Modell
+    user = models.OneToOneField(User, on_delete=models.CASCADE) # 1:1 Beziehung: User <-> Profil, Cascade: wenn User gelöscht wird, dann lösche auch das Profil (Einwegfunktion), wenn Profil gelöscht wird, dann wird der User nicht gelöscht
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics') # default und dir Einstellung
+
+    def __str__(self):
+        return f'{self.user.username} Profile'  # Profil mit dem vorhandenen Username wird ausgegeben 
+
+    
