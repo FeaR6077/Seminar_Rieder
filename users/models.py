b898412f8e9ccea1c2c040dd8ed4ef657a72ef70
Baschtl1993 <MBastian93@googlemail.com>
2020-11-24 16:04:39 +0100

    Profilerweiterung

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User #User-Modell wird von Django zur Verfügung gestellt
+from PIL import Image
 
 # Create your models here.
 
@@ -11,4 +12,12 @@ class Profile(models.Model): # erbt von import. Modell
     def __str__(self):
         return f'{self.user.username} Profile'  # Profil mit dem vorhandenen Username wird ausgegeben 
 
-    
+    def save(self):
+        super().save()
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
